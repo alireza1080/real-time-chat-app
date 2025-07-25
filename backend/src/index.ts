2e0f986e8e127da23a546ab87836aa13913c281b
Alireza Aberoumand <maya7192@yahoo.com>
2025-07-24 22:03:26 -0700

    Update dependencies to include Prisma and enhance server startup with database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import { connectToDatabase } from './services/database.service.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  await connectToDatabase();
 });
